c85bcdbec9270815a55417df9fe9778b8327684c
Aw3som3Ash3 <ashtonadkins1120@gmail.com>
2023-12-06 11:56:30 -0500

    fixed bullet
    stopped bullets from destroying themselves on each other

---
+++ JLHJ/Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/Bullet.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletSpeed = 3f;
+    public float bulletMove;
+
+    void Start()
+    {
+        bulletMove = bulletSpeed * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(0, 0, bulletMove);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ghost")
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+        else if (other.tag == "Player")
+        {
+            return;
+        }
+        else if (other.tag == "bullet")
+        {
+            return;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        Destroy(gameObject, 3f);
+    }
+}
